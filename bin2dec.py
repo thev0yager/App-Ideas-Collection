4440ac61514a46e6c70e86ee219615f0a1370966
thev0yager <65251242+thev0yager@users.noreply.github.com>
2022-03-29 22:24:11 -0500

    Add files via upload
    Added a gui to the application using tkinter!

--- bin2dec.py
+++ bin2dec.py
@@ -1,8 +1,20 @@
-value = input("Type 8 digit binary: ")
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
 
+# root window
+root = tk.Tk()
+root.geometry("300x150")
+root.resizable(False, False)
+root.title('Binary 2 Decimal')
+
+# function value
+value = tk.StringVar()
+
+# binary conversion
 def bin2dec():
     
-    binNum = int(value)
+    binNum = int(value.get())
     decNum = 0
     power = 0
 
@@ -11,9 +23,28 @@ def bin2dec():
         binNum //= 10
         power += 1
 
-    if power > 8:
-        return print("Wrong number of digits")
-    else:
-        return print(decNum)
+    msg = f'Your converted value is: {decNum}'
 
-bin2dec()
+    if power > 8:
+        showinfo(title='Error', message='Wrong number of digits')
+    else:
+        showinfo(title='Information', message=msg)
+
+# app frame
+app = ttk.Frame(root)
+app.pack(padx=10, pady=10, fill='x', expand=True)
+
+# input value label
+value_label = ttk.Label(app, text="Enter Binary String: ")
+value_label.pack(fill='x', expand=True)
+
+# input entry field
+value_entry = ttk.Entry(app, width=8, textvariable=value)
+value_entry.pack(fill='x', expand=True)
+value_entry.focus()
+
+# submit button
+submit_button = ttk.Button(app, text="Submit", command=bin2dec)
+submit_button.pack(fill='x', expand=True, pady=10)
+
+root.mainloop()
